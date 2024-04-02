7e4e983a992a446514f1b4e52c5114a76774f4a3
eribertokervalt83 <165734358+eribertokervalt83@users.noreply.github.com>
2024-04-01 21:23:01 -0300

    Update Cartorio10.c
    Incluso no sistema a opção de saída do usuário

--- Cartorio10.c
+++ Cartorio10.c
@@ -138,7 +138,8 @@ int main()
     	printf("escolha a opção desejada do menu:\n\n");
 	    printf("\t1 - Registar nomes\n");
     	printf("\t2 - Consultar nomes\n ");
-    	printf("\t3 - Deletar nomes\n\n"); 
+    	printf("\t3 - Deletar nomes\n\n");
+        printf("\t4 - Sair do sistema\n\n");
     	printf("opção: ");//Fim do menu
 	
     	scanf("%d",  &opcao); //armazenando a escolha do usuário
@@ -158,6 +159,12 @@ int main()
         	case 3:
         	deletar();
 	    	break;
+
+		case 4:
+		printf("Obrigado por ultilizar o sistema"\n");
+		return 0;
+		break;
+			
 	    		
 	    	default:
 	    	printf("Essa opção não está disponível!\n");
